31f7e760746e61ed95b6652af88abb2ccef89c12
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2023-12-28 12:07:59 +0300

    operators and type conversion

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 class Hello
 {
   public static void main(String a[]) {
-    System.out.println("Hello, World!");
+    // making number easier to read
+    int num = 20_000_000; 
+    byte num1 = 3;
+    int result = num*num1; //type promotion
+    System.out.println(result); 
   }
 }
