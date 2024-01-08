5ec5b0d55f8db581d2ae4b2d363db598fd0497ff
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2024-01-08 09:22:57 +0300

    updated animal.java

--- first/Animal.java
+++ first/Animal.java
@@ -4,22 +4,22 @@ public class Animal {
   String name;
   String colour;
   int legs;
-  int numberOfEars;
+  static int numberOfEars = 2;
 
   public static void main(String[] args) {
     Animal dog = new Animal();
-    dog.printAtrributes("Scooby", "Black", 4, 2);
+    dog.printAtrributes("Scooby", "Black", 4);
     Animal cat = new Animal();
-    cat.printAtrributes("Tom", "Brown", 4, 2);
+    cat.printAtrributes("Tom", "Brown", 4);
     Animal person = new Animal();
-    person.printAtrributes("Malcolm", "Light-skinned",2, 2);
+    person.printAtrributes("Malcolm", "Light-skinned",2);
   }
 
-  public void printAtrributes(String name, String colour, int legs, int numberOfEars) {
+  public void printAtrributes(String name, String colour, int legs) {
     this.name = name;
     this.colour = colour;
     this.legs = legs;
-    this.numberOfEars = numberOfEars;
-    System.out.println("Name is "+ this.name + "\nColour is: "+ this.colour + "\nLegs are: "+ this.legs + "\nNumber of ears are: "+ this.numberOfEars + "\n");
+    // this.numberOfEars = numberOfEars;
+    System.out.println("Name is "+ this.name + "\nColour is: "+ this.colour + "\nLegs are: "+ this.legs + "\nNumber of ears are: "+ numberOfEars + "\n");
   }
 }
