3b21edea18fb543a7e2045c0f76722b022ff62ca
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2024-02-19 14:11:04 +0300

    more work on overriding

--- lesson5_3.java
+++ lesson5_3.java
@@ -1,5 +1,10 @@
 // Java Program to implement
 // Method Overriding
+
+/*
+ * Note: Static methods belong to the class rather than an instance of the class. They are associated with the class itself and not with the instances of the class.
+ * Note: final methods cannot be overriden.
+ */
 import java.io.*;
 
 // Base Class
@@ -9,15 +14,27 @@ class Animal {
 		System.out.println("eat() method of base class");
 		System.out.println("eating.");
 	}
+  final void talk()
+  {
+    System.out.println("talk() method of base class");
+    System.out.println("talking.");
+  }
 }
 
 // Inherited Class
 class Dog extends Animal {
-	void eat()
+  // This is not considered an override; it's a separate static method(polymorphism).
+	static void eat()
 	{
 		System.out.println("eat() method of derived class");
 		System.out.println("Dog is eating.");
 	}
+
+  void talk()
+  {
+    System.out.println("talk() method of derived class");
+    System.out.println("Dog is talking.");
+  }
 }
 
 class MethodOverridingEx {
@@ -30,9 +47,13 @@ class MethodOverridingEx {
 		d1.eat();
 		a1.eat();
 
+    d1.talk();
+    a1.talk();
+
 		Animal animal = new Dog();
 		// eat() method of animal class is overridden by
 		// base class eat()
 		animal.eat();
+    animal.talk();
 	}
 }
