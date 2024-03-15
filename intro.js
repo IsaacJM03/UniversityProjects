bc285d65673eacd77e014707a8429aa5ca7dc489
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2024-03-15 11:40:46 +0300

    js basics

---
+++ intro.js
@@ -0,0 +1,10 @@
+function msg() {
+  alert("type here");
+}
+
+function sum() {
+  var num1 = prompt("Enter number 1");
+  var num2 = prompt("Enter number 2");
+  var sum = parseInt(num1) + parseInt(num2);
+  alert("The sum is: "+sum);
+}
