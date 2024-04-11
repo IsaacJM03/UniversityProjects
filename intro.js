f6eb4e090412ee09b203dc49c956430c9b6fd0f4
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2024-04-11 10:55:50 +0300

    added more js practice

--- intro.js
+++ intro.js
@@ -1,5 +1,31 @@
 function msg() {
-  alert("type here");
+  // confirm("Are you good?");
+  // alert("type here");
+//   p=prompt("Ankara'nın plaka numarası nedir?", " ")
+//   if(p=="yo")
+//   {
+//   alert("Doğru")
+//   }
+//   else
+//   {
+//   alert("Yanlış")
+// }
+try{
+  var num = prompt("Enter a number (1-2):", "1");
+  // You can throw exception of any type
+  if (num == "1")
+  throw "Some Error Message";
+  else
+  if (num == "2")
+  throw 123;
+  else
+  throw new Error ("Invalid input");
+  } catch( err ) {
+  alert(typeof(errMsg) + "\n" + err);
+  // instanceof operator checks if err is an Error object
+  if (err instanceof Error)
+  alert("Error Message: " + err.message);
+  }
 }
 
 function sum() {
